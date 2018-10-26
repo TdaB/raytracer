ce7dfd3e0784e31885e148dd5f920a9ba4baa235
TdaB <tmegantz@gmail.com>
2018-10-25 19:05:27 -0700

    Added main.cpp as new entry point

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include "Dependencies\glew\glew.h"
-#include "Dependencies\freeglut\freeglut.h"
+#include "Dependencies\glew-2.1.0\include\GL\glew.h"
+#include "Dependencies\freeglut\include\GL\freeglut.h"
 #include <iostream>
 #include <limits>
 #include <ctime>
 #include <vector>
 #include "objects.h"
-#include "utils.h"
 #include "scenes.h"
 #include "raytracer.h"
 
@@ -19,43 +18,46 @@ void render_pixel(int x, int y, Color c) {
 	glEnd();
 }
 
-void render_scene(Scene s) {
-    clock_t begin = clock();
-    
-    Raytracer r = Raytracer(s);
-    for (int x = -(s.width / 2); x < s.width / 2; x++) {
-        for (int y = -(s.height / 2); y < s.height / 2; y++) {
-            Point pixel = Point(x, y, 0);
-            Color fill = r.trace(s.camera, pixel, s.bounces);
-            render_pixel(x, y, fill);
-        }
-    }
-    glFlush();
-    
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    cout << "RENDERING TIME: " << elapsed_secs << " SECONDS";
-}
+void render_scene() {
+	Raytracer r = Raytracer(get_scene());
+	int width = r.scene.width;
+	int height = r.scene.height;
+	int camera_z = r.scene.camera_z;
+	int bounces = r.scene.bounces;
 
-void init_glut_and_render(int argc, char **argv, Scene s) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-    glutInitWindowPosition(0, 0);
-    glutInitWindowSize(s.width, s.height);
-    glutCreateWindow("Dat Raytracer");
-    glClearColor(0, 0, 0, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glEnable(GL_POINT_SMOOTH);
-    // Define coords in viewing area
-    gluOrtho2D(-(s.width / 2.0), s.width / 2.0, -(s.height / 2.0), s.height / 2.0);
-    // 2 days of my life gone from this damn option
-    glViewport(0, 0, s.width, s.height);
-    glutDisplayFunc(render_scene(s));
-    glutMainLoop();
+	clock_t begin = clock();
+
+	for (int x = -(width / 2); x < width / 2; x++) {
+		for (int y = -(height / 2); y < height / 2; y++) {
+			Point camera = Point(x, y, camera_z - 1);
+			Point pixel = Point(x, y, camera_z);
+			Color fill = r.trace(camera, pixel, bounces);
+			render_pixel(x, y, fill);
+		}
+	}
+
+	glFlush();
+
+	clock_t end = clock();
+	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	cout << "RENDERING TIME: " << elapsed_secs << " SECONDS";
 }
 
 int main(int argc, char **argv) {
-    init_glut_and_render(argc, argv, get_scene());
+	Scene s = get_scene();
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(s.width, s.height);
+	glutCreateWindow("Faster than a speeding slug...");
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_POINT_SMOOTH);
+	// Define coords in viewing area
+	gluOrtho2D(-(s.width / 2.0), s.width / 2.0, -(s.height / 2.0), s.height / 2.0);
+	// 2 days of my life gone from this damn option
+	glViewport(0, 0, s.width, s.height);
+	glutDisplayFunc(render_scene);
+	glutMainLoop();
 	return 0;
 }
-
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H 
+#define MAIN_H
+#include "objects.h"
+
+
+void render_pixel(int x, int y, Color c);
+void render_scene();
+
+#endif
